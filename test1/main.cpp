30065e1795dc5060818365bf5023bbd9c6a94090
iLecus <1617815602@qq.com>
2018-05-10 20:55:07 +0800

    modify main.cpp & add img

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,9 +1,12 @@
 #include<opencv2\opencv.hpp>
+#include<iostream>
 using namespace cv;
+using namespace std;
 int main()
 {
 	Mat picture = imread("wallpaper.jpg");//图片必须添加到工程目录下
 										  //也就是和test.cpp文件放在一个文件夹下！！！
 	imshow("测试程序", picture);
 	waitKey(20150901);
+	cout << "push to github again" << endl;
 }
